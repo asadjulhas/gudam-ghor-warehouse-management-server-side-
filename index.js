6ecbb176aff7486618b52db29736b1a3e4f7fba2
asadjulhas <fahim0760@gmail.com>
2022-05-06 09:28:48 +0600

    Change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const ObjectId = require('mongodb').ObjectId;
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 
