7b7fb9e3d52399c94d0d974fcda77b266aef8b01
asadjulhas <fahim0760@gmail.com>
2022-05-19 05:35:07 +0600

    add router for task complete

--- index.js
+++ index.js
@@ -152,6 +152,18 @@ const result = await taskCollections.deleteOne(query);
 res.send(result);
  });
 
+ // update task
+
+ app.put('/complete-task/:id', async (req, res) => {
+  const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+  const updateDoc = {
+    $set: {status: 2}
+  };
+  const result = await taskCollections.updateOne(query, updateDoc);
+  res.send({result})
+})
+
 
 
   }
